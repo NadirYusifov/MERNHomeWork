158801f3057be598f585b684327177480cb11857
Nadir Yusifov <nadiryusifov448@gmail.com>
2025-04-23 15:59:11 +0400

    Create new book

--- 4NodeJSHomework/controllers/createNewBook.js
+++ 4NodeJSHomework/controllers/createNewBook.js
@@ -1,21 +1,32 @@
-import { readFileSync } from "fs";
+import { randomUUID } from "crypto";
+import { readFileSync, writeFileSync } from "fs";
 
 const createNewBook = (req, res) => {
     try {
         const data = readFileSync("./db.json", "utf-8");
         const { books } = JSON.parse(data);
-        const 
-        // console.log(books);
-        res.status(200).send(books);
-        
+        const newBook = req.body;
+        newBook.id = randomUUID();
+        books.push(newBook)
+        const updatedData = JSON.stringify({ books: newBook }, null, 2)
+        writeFileSync("./db.json", updatedData, "utf-8")
+        res.status(201).send({
+            status: 201,
+            message: "Book created successfully"
+        })
+
+        // Burda konsolda newBook'u görə bilmədim.
+        // console.log(newBook);
+
     } catch (error) {
-        res.status(500).send({
+        res.status(500).send(({
             status: 500,
             message: "Error",
-        });
-    }
+        }));
 
-    console.log(req.body);
+        res.send("Ok")
+
+    }
 }
 
 export default createNewBook;
